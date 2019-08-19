d09e1e50b835c6a7424b52b69b2a1f71242bfa52
nd98 <bptpgt@gmail.com>
2019-08-20 01:30:23 +0530

    most of the work is completed Few Minor Errors

--- BackPropagation.py
+++ BackPropagation.py
@@ -6,17 +6,14 @@ from keras.models import Sequential
 from keras.layers import Dense
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.externals import joblib
+import os
 
-def BackProp(col_predict,no_of_output_para,input_par,link,epoch):
 
-    col_predict = 1
-    no_of_output_para = 5
-    input_par = 25
-    link = "https://github.com/Nd98/Neural-Network-Dataset/blob/master/Test_Properties_5.xls?raw=true"
-    epoch = 100
+sc_array = []
 
-    # dataset = pd.read_csv("/content/drive/My Drive/nayan/Teaning_data_set.csv")
-    # dataset = pd.read_csv("https://raw.githubusercontent.com/Nd98/Neural-Network-Dataset/master/Teaning%20Data%20set.csv")
+def BackProp(col_predict,no_of_output_para,input_par,link,epoch,units,tf):
+
     dataset = pd.read_excel(link)
     X = dataset.iloc[:,no_of_output_para + 1:dataset.values[0].size].values
     y = dataset.iloc[:,col_predict].values
@@ -29,19 +26,21 @@ def BackProp(col_predict,no_of_output_para,input_par,link,epoch):
     X_train = sc.fit_transform(X_train)
     X_test = sc.transform(X_test)
 
+    sc_array.append(sc)
+
     classifier = Sequential()
 
 
-    classifier.add(Dense(units = 5, kernel_initializer = 'uniform', activation = 'relu', input_dim = input_par))
+    classifier.add(Dense(units = units, kernel_initializer = 'uniform', activation = tf, input_dim = input_par))
 
 
-    classifier.add(Dense(units = 5, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = units, kernel_initializer = 'uniform', activation = tf))
 
 
-    classifier.add(Dense(units = 5, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = units, kernel_initializer = 'uniform', activation = tf))
 
 
-    classifier.add(Dense(units = 1, kernel_initializer = 'uniform' ,activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform' ,activation = tf))
 
 
     classifier.compile(optimizer = 'adam', loss = 'mean_absolute_error', metrics = ['mean_absolute_error'])
@@ -50,7 +49,43 @@ def BackProp(col_predict,no_of_output_para,input_par,link,epoch):
     classifier.fit(X_train, y_train, batch_size = 10, epochs = epoch)
     #epochs=500, batch_size=10, validation_split = 0.2, callbacks=callbacks_list
 
-    y_pred = classifier.predict(X_test)
+    joblib.dump(classifier,link+"-"+str(col_predict)+".pkl")
 
-    print(y_pred)
+def predict(arr,col_predict,link):
 
+
+    classifier = joblib.load(link+"-"+str(col_predict)+".pkl")
+
+    # y_pred = classifier.predict(X_test)
+
+    # # p = np.array([[60,1559,9.72,20.67,70.25,3,6.7,5.26,22.42,9.30,23.67,79.44,10.63,9.5,25.17,73.75,0.09,0.059,0.962,1.274,0.01,0.039,18.538,8.27,0.235
+    # # ]]) #10
+    p = np.array([[69,1563,9.72,19.50,72.25,4,6.67,5.07,22.21,9.25,23.17,69.75,10.42,9.5,25.17,73.75,0.09,0.053,0.975,1.196,0.008,0.037,18.211,8.11,0.203
+    ]]) #11
+    
+
+    sc = sc_array[col_predict-1]
+    p = sc.transform(p)
+    p = classifier.predict(p)
+    print (p)
+    return p
+
+no_of_output_para = 5
+input_par = 25
+# link = "https://github.com/Nd98/Neural-Network-Dataset/blob/master/Test_Properties_5.xls?raw=true"
+epoch = 100
+units = 100
+tf = 'relu'
+link = os.path.abspath("instance/Test.xls")
+x = no_of_output_para
+while(x>0):
+    col_predict = x
+    BackProp(col_predict,no_of_output_para,input_par,link,epoch,units,tf)
+    x-=1
+
+sc_array.reverse()
+x = no_of_output_para
+while(x>0):
+    col_predict = x
+    predict([],col_predict,link)
+    x-=1
--- api.py
+++ api.py
@@ -1,26 +1,132 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 import pandas as pd
 import os, json
 from werkzeug.utils import secure_filename
+from flask_cors import CORS
+import BackPropagation as bp
 
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/upload', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        inp_par = 6
         f = request.files['file']
         fName = f.filename
         f.save(os.path.join(app.instance_path,secure_filename(fName)))
-        data_xls = pd.read_excel(f)
-        print(data_xls)
-        cols = data_xls.columns[1:inp_par]
-        arr = []
-        for col in cols:
-            arr.append(col)
-        return jsonify(arr)
+        session['file-name'] = fName
+        session['input-para'] = 25
+        session['output-para'] = 5
+        print(vars(session))
+        return "File Uploaded Successfully"
     return "Method is not supported"
 
+@app.route('/neurons', methods=['POST'])
+def getNeurons():
+    fileName = ""
+    if request.method == 'POST':
+
+        if 'input-para' in session:
+            input_para = session['input-para']
+        
+        print(session['file-name'])
+
+        return jsonify(input_para)
+    return "Method is not supported"
+
+@app.route('/controlData', methods=['POST'])
+def setControlData():
+    fileName = ""
+    if request.method == 'POST':
+        if 'file-name' in session:
+            fileName = session['file-name']
+        if 'input-para' in session:
+            input_para = session['input-para']
+        if 'output-para' in session:
+            output_para = session['output-para']
+        
+        print(session['file-name'])
+        ta = request.form["ta"]
+        tf = request.form["tf"]
+        neurons = request.form["neurons"]
+
+        session["ta"] = ta
+        session["tf"] = tf
+        session["neurons"] = neurons
+
+        f = os.path.abspath("instance/"+fileName)
+        if ta == "bp":
+            x = output_para
+            while(x>0):
+                bp.BackProp(x,output_para,input_para,f,100,100,tf)
+                x-=1
+
+        return "Model Trained"
+    return "Method is not supported"
+
+@app.route('/getParam', methods=['POST'])
+def getParam():
+    fileName = ""
+    if request.method == 'POST':
+        if 'file-name' in session:
+            fileName = session['file-name']
+        if 'input-para' in session:
+            input_para = session['input-para']
+        if 'output-para' in session:
+            output_para = session['output-para']
+        
+        print(session['file-name'])
+
+        f = os.path.abspath("instance/"+fileName)
+        
+        data_xls = pd.read_excel(f)
+        cols_input = data_xls.columns[1 + output_para: 1 + output_para + input_para]
+        cols_output = data_xls.columns[1:output_para+1]
+        arr1 = []
+        arr2 = []
+
+        for col in cols_input:
+            arr1.append(col)
+        for col in cols_output:
+            arr2.append(col)
+        
+        arr = []
+        arr.append(arr1)
+        arr.append(arr2)
+
+        return jsonify(arr)
+        
+    return "Method is not supported"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    fileName = ""
+    if request.method == 'POST':
+        if 'file-name' in session:
+            fileName = session['file-name']
+        if 'output-para' in session:
+            output_para = session['output-para']
+        
+        print(session['file-name'])
+        
+        f = os.path.abspath("instance/"+fileName)
+        arr = []
+        result = []
+
+        ta = session["ta"]
+
+        if ta == "bp":
+            x = output_para
+            while(x>0):
+                result.append(bp.predict(arr,x,f))
+                x-=1
+
+        return jsonify(result.reverse())
+    return "Method is not supported"
+
+
 if __name__ == '__main__':
+    app.secret_key = os.urandom(24)
     app.run(debug=True)
